87535a9146f4010e46cbea61be7cec422d66d8bb
Sarvang Chaturvedi <sarvangchaturvedi1@gmail.com>
2026-02-03 23:51:33 +0530

    Hero section added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Button } from './components/ui/button'
 import Navbar from "./components/Navbar.jsx"
 import Login from './pages/Login'
+import HeroSection from './pages/student/HeroSection'
 
 function App() {
 
   return (
     <div>
       <Navbar/>
+      <HeroSection />
       <Login />
     </div>
   )
