63240c81860da3c54c7a04bb68fb7fb38a5bf903
Sarvang Chaturvedi <sarvangchaturvedi1@gmail.com>
2026-03-30 00:14:03 +0530

    Ready for deployment

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3000
 // default middlewares
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
 
 
 // apis
